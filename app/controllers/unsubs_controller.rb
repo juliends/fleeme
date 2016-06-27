366496b6c7cb2024b1c1e5033f943bd1b5e3d730
Julien Da Silva <julien.da.silva@hotmail.com>
2016-06-27 18:00:00 +0200

    testing the form

--- app/controllers/unsubs_controller.rb
+++ app/controllers/unsubs_controller.rb
@@ -5,7 +5,25 @@ class UnsubsController < ApplicationController
     @unsub = Unsub.new
     @user = User.new
 
-    # initialize_hash
-    # @fields = generate_fields(@hash_service)
+    initialize_hash
+    @fields = generate_fields(@hash_service)
+    #loading the form_specs from json file
+  end
+
+  private
+
+  def initialize_hash
+    file = File.read('app/assets/form_specs/ugc.json')
+    @hash_service = JSON.parse(file, symbolize_names: :true)[:fields]
+  end
+
+  def generate_fields(hash)
+    hash.each do |field|
+      @fieldname = field[:name]
+    end
+  end
+
+  def unsub_params
+    params.require(:unsub).permit(:form_complete, :photo)
   end
 end

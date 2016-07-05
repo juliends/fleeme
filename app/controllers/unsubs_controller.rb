f6f57ae5c1235cdef30a8edbfaba59642d8a1751
Julien Da Silva <julien.da.silva@hotmail.com>
2016-07-05 16:04:15 +0200

    Add figaro to protect secret data

--- app/controllers/unsubs_controller.rb
+++ app/controllers/unsubs_controller.rb
@@ -12,7 +12,12 @@ class UnsubsController < ApplicationController
   end
 
   def create
+    api_url = 'http://api.icndb.com/jokes/random?escape=javascript'
 
+    open(api_url) do |stream|
+    quote = JSON.parse(stream.read)
+    puts quote['value']['joke']
+end
   end
 
   private

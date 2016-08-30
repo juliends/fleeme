17123eaa0aa73f43ed63899f8d421bbb0f10e8bd
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-30 17:57:38 +0200

    working on webhook

--- app/controllers/unsubs_controller.rb
+++ app/controllers/unsubs_controller.rb
@@ -8,17 +8,8 @@ class UnsubsController < ApplicationController
 
   def create
     @unsub = Unsub.new
-    result = open('http://requestb.in/1gf6ago1?inspect')
-    result.lines { |f| f.each_line {|line| p line} }
+    
 
-    api_url = 'https://api.typeform.com/v1/form/QyqJ49?key=TYPEFORM_KEY'
-
-    open(api_url) do |stream|
-      quote = JSON.parse(stream.read)
-      puts quote['value']['joke']
-    end
-
-    raise
 
     if @unsub.save
       redirect_to unsub_path(@unsub)

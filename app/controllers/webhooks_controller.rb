558cc9225905546c201acaee33af0347f96499a5
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-01 17:52:11 +0200

    keep on working

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -12,41 +12,41 @@ class WebhooksController < ApplicationController
       @data = params.as_json
     end
 
-    if @data["form_response"]["form_id"] == "QyqJ49"
-      ugc
-    else 
-      # On pourra ici ajouter les id d' autres forms Typeform
-      false
-    end
-    render nothing: true
+    Unsub.create(form_complete: @data, user_id: @data["form_response"])
+    # if @data["form_response"]["form_id"] == "QyqJ49"
+    #   ugc
+    # else 
+    #   ugc
+    # end
+
+    render nothing: true, status: 200
   end
 
-  def ugc
-    @answers = answers_to_hash(@data["form_response"]["answers"]) 
-    @user = User.new
-    @user.firstname = @answers["25108292"]
-    @user.lastname = @answers["25108413"]
-    @user.email = @answers["25423473"]
-    @user.address = @answers["25423131"]
-    @user.zipcode = @answers["25424220"]
-    @user.city = @answers["25424218"]
-    @user.session_id = @data["form_response"]["hidden"]["session"]
-    @user.save
-    @service = @data["form_response"]["hidden"]["service"]
-    @unsub = Unsub.create!(user: @user, service_id: @service, form_complete: @data)
-  end
+  # def ugc
+  #   @answers = answers_to_hash(@data["form_response"]["answers"]) 
+  #   @id = @data["form_response"]["hidden"]["id"].to_i
+  #   @user = User.where(id: @id)
+  #   @user.firstname = @answers["25108292"]
+  #   @user.lastname = @answers["25108413"]
+  #   @user.email = @answers["25423473"]
+  #   @user.address = @answers["25423131"]
+  #   @user.zipcode = @answers["25424220"]
+  #   @user.city = @answers["25424218"]
+  #   @service = @data["form_response"]["hidden"]["service"]
+  #   @unsub = Unsub.create!(user: @user, service_id: @service, form_complete: @data)
+  # end
 
-  def answers_to_hash(array)
-    hash = {} 
-    array.each do |q|
-      types = ['text', 'email']
-      type = q['type']
-      if types.include?(type)
-        hash[q["field"]["id"]] = q[type]
-      elsif type == 'choice'
-        hash[q['field']['id']] = q["choice"]
-      end
-    end
-    return hash
-  end
+  # def answers_to_hash(array)
+  #   hash = {} 
+  #   array.each do |q|
+  #     types = ['text', 'email']
+  #     type = q['type']
+  #     if types.include?(type)
+  #       hash[q["field"]["id"]] = q[type]
+  #     elsif type == 'choice'
+  #       hash[q['field']['id']] = q["choice"]
+  #     end
+  #   end
+  #   return hash
+  # end
 end

92f1c3b8ed3f8e2f8349ba81cb2213d6f96253bb
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-06 12:13:35 +0200

    good job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901111517) do
+ActiveRecord::Schema.define(version: 20160906100553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,5 +113,5 @@ ActiveRecord::Schema.define(version: 20160901111517) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "unsubs", "services"
-  add_foreign_key "unsubs", "users"
+  add_foreign_key "unsubs", "users", on_delete: :cascade
 end
